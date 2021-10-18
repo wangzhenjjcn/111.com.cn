12b883cc9db3dbf7e554966b803d370478a0d0b4
WangZhen <wangzhenjjcn@gmail.com>
2021-10-18 23:43:35 +0800

    1

---
+++ src/App.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import Chrome
+import www_111_com_cn
+
+
+
+from Chrome import Chrome
+
+
+class App():
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    chrome=Chrome()
+    www_111_com_cn=www_111_com_cn()
